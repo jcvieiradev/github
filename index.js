9198ab7dda7bae6550a756c21d22519b13c4a07a
jcvieiradev <jcvieiradev@gmail.com>
2023-09-02 10:48:39 -0300

    13 commit

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const configuracao = require("./config/config");
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({ path: path.join(__dirname, 'config', '.env') });
 
 
 console.log(configuracao)
+console . log ( process.env.NODE_ENV_USUARIOs )
